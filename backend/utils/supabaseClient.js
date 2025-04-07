2ad9349c66f9b096213ffa105274e0a829c3066b
Kalash Satypal <Kalashsatypal@gmail.com>
2025-04-07 13:37:10 +0530

    2

--- backend/utils/supabaseClient.js
+++ backend/utils/supabaseClient.js
@@ -1,8 +1,11 @@
-const { createClient } = require('@supabase/supabase-js');
+// supabaseClient.js
+import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
-);
+const supabaseUrl = 'https://qqkbfunfwixklqodgzwt.supabase.co'; // Replace with your actual URL
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFxa2JmdW5md2l4a2xxb2Rnend0Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MzMzMjIzMywiZXhwIjoyMDU4OTA4MjMzfQ.RilAbY8TFuq18lFuBfCp8f7N2qocG69rPUYry-MdpDs'; // Replace with your anon key
 
-module.exports = supabase;
+if (!supabaseKey) {
+  throw new Error('supabaseKey is required.');
+}
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
